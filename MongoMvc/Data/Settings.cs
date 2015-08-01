2a72824943912d3451e6a0713ed0943f290180c0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-08-01 20:40:29 +0200

    Update MongoDB client configuration class
    - change directory
    - add Collection key

--- MongoMvc/Data/Settings.cs
+++ MongoMvc/Data/Settings.cs
@@ -1,10 +1,11 @@
 using System;
 using MongoDB.Driver;
 
-namespace MongoMvc
+namespace MongoMvc.Data
 {
     public class Settings
     {
+        public string Collection { get; set; }
         public string Database { get; set; }
         public string MongoConnection { get; set; }
     }
