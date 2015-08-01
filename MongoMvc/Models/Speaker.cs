a30f872792c87bf0eaf47a20dc73bd40cfc748cd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-08-01 20:35:54 +0200

    Use MongoDB C# conventions to map object attributes

--- MongoMvc/Models/Speaker.cs
+++ MongoMvc/Models/Speaker.cs
@@ -1,5 +1,4 @@
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace MongoMvc.Models
@@ -7,21 +6,10 @@ namespace MongoMvc.Models
     public class Speaker
     {
         public ObjectId Id { get; set; }
-
-        [BsonElement("blog")]
         public string Blog { get; set; }
-
-        [BsonElement("first")]
         public string First { get; set; }
-
-        [BsonElement("last")]
         public string Last { get; set; }
-
-        [BsonElement("title")]
         public string Title { get; set; }
-
-        [BsonElement("twitter")]
         public string Twitter { get; set; }
-
     }
 }
