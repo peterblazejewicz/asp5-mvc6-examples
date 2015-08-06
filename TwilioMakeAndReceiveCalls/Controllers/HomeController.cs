30032e20d1de4ef95d40e7ef901ae037c1ddaee0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-08-06 22:40:45 +0200

    Add MakeAndReceive Calls by Twilio

---
+++ TwilioMakeAndReceiveCalls/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Twilio;
+using Twilio.TwiML;
+
+namespace TwilioMakeAndReceiveCalls.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult MakeCall(string number)
+        {
+            var client = new TwilioRestClient("", "");
+            var results = client.InitiateOutboundCall("Twilio.MyNumber", "number", "http://www.televisiontunes.com/uploads/audio/Star%20Wars%20-%20The%20Imperial%20March.mp3");
+            if (results.RestException != null)
+            {
+                return Content(results.RestException.Message);
+            }
+            return Content("The call has been is initiated");
+        }
+
+        [HttpGet]
+        public IActionResult ReceiveCall()
+        {
+            var twiml = new TwilioResponse();
+            twiml.Say("You are calling Marcos Placona").Dial("MyOwnNumber");
+            return Content(twiml.ToString());
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ TwilioMakeAndReceiveCalls/README.md
@@ -0,0 +1,20 @@
+# TwillioMakeAndReceiveCalls
+
+```
+➜  TwilioMakeAndReceiveCalls git:(master) ✗ dnu install Twilio.Mvc
+Microsoft .NET Development Utility Mono-x64-1.0.0-beta6-12256
+
+  CACHE https://www.nuget.org/api/v2/
+  CACHE https://www.nuget.org/api/v2/FindPackagesById()?id='Twilio.Mvc'
+Twilio.Mvc.3.1.15 was added to project.json.
+Restoring packages for /Users/piotrblazejewicz/git/asp5-mvc6-examples/TwilioMakeAndReceiveCalls/project.json
+  CACHE https://www.nuget.org/api/v2/
+Writing lock file /Users/piotrblazejewicz/git/asp5-mvc6-examples/TwilioMakeAndReceiveCalls/project.lock.json
+Restore complete, 2902ms elapsed
+
+NuGet Config files used:
+    /Users/piotrblazejewicz/.config/NuGet/NuGet.Config
+
+Feeds used:
+    https://www.nuget.org/api/v2/
+```
