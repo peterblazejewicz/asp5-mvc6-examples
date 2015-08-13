cc33134cbee4276ee8c24198dd85b2d31dd6240c
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-08-13 23:40:40 +0200

    Scaffold MVC project for ProgressBarComponent

---
+++ ProgressBarComponent/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ProgressBarComponent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
