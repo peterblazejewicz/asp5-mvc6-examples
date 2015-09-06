72bd43d11ab7f6709655dd8d39a99c0b9b7086a8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-06 23:19:43 +0200

    Update client side implementation
    - update deps
    - update imports
    - optimize wwwroot installation

--- EFGetStarted.AspNet5/gulpfile.js
+++ EFGetStarted.AspNet5/gulpfile.js
@@ -1,11 +1,13 @@
 /// <binding Clean='clean' />
 'use strict';
+/* jshint node: true */
 var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
   uglify = require("gulp-uglify"),
-  project = require("./project.json");
+  project = require("./project.json"),
+  mainBowerFiles = require("gulp-main-bower-files");
 
 var paths = {
   webroot: "./" + project.webroot + "/"
@@ -17,6 +19,7 @@ paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.lib = paths.webroot + "lib/";
 
 gulp.task("clean:js", function(cb) {
   rimraf(paths.concatJsDest, cb);
@@ -24,6 +27,10 @@ gulp.task("clean:js", function(cb) {
 
 gulp.task("clean:css", function(cb) {
   rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean:lib", function(cb) {
+  rimraf(paths.lib, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
@@ -45,3 +52,9 @@ gulp.task("min:css", function() {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("bower", ["clean:lib"], function() {
+  return gulp.src("./bower.json")
+    .pipe(mainBowerFiles())
+    .pipe(gulp.dest(paths.lib));
+});
