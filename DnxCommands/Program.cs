f35f98167e8f5f4d745463aeea044e41795deb06
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-10 23:32:29 +0200

    Scaffold console application for DnxCommands

---
+++ DnxCommands/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnxCommands
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Wait, why World");
+            Console.ReadLine();
+        }
+    }
+}
