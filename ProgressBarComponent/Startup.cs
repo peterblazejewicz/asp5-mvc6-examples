c0e7406c59e0cdfeecaa2ecf838a5b29ccbef000
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-16 21:36:59 +0200

    Update ProgressBar example to beta7

--- ProgressBarComponent/Startup.cs
+++ ProgressBarComponent/Startup.cs
@@ -3,75 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
+using Microsoft.Dnx.Runtime;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using Microsoft.Framework.Runtime;
 
 namespace ProgressBarComponent
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
-        {
-            // Setup configuration sources.
-            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
-                .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
 
-        public IConfiguration Configuration { get; set; }
+  public class Startup
+  {
+      public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+      {
+          // Setup configuration sources.
+          var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+              .AddJsonFile("config.json")
+              .AddEnvironmentVariables();
+          Configuration = builder.Build();
+      }
 
-        // This method gets called by the runtime.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add MVC services to the services container.
-            services.AddMvc();
+      public IConfigurationRoot Configuration { get; set; }
 
-            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
-            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
-            // services.AddWebApiConventions();
-        }
+      // This method gets called by the runtime.
+      public void ConfigureServices(IServiceCollection services)
+      {
+          // Add MVC services to the services container.
+          services.AddMvc();
 
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.MinimumLevel = LogLevel.Information;
-            loggerFactory.AddConsole();
+          // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+          // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+          // services.AddWebApiConventions();
+      }
 
-            // Configure the HTTP request pipeline.
+      // Configure is called after ConfigureServices is called.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+      {
+          loggerFactory.MinimumLevel = LogLevel.Information;
+          loggerFactory.AddConsole();
+          loggerFactory.AddDebug();
 
-            // Add the following to the request pipeline only in development environment.
-            if (env.IsDevelopment())
-            {
-                app.UseErrorPage();
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
-            }
+          // Configure the HTTP request pipeline.
 
-            // Add static files to the request pipeline.
-            app.UseStaticFiles();
+          // Add the following to the request pipeline only in development environment.
+          if (env.IsDevelopment())
+          {
+              app.UseErrorPage();
+          }
+          else
+          {
+              // Add Error handling middleware which catches all application specific errors and
+              // send the request to the following path or controller action.
+              app.UseErrorHandler("/Home/Error");
+          }
 
-            // Add MVC to the request pipeline.
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+          // Add static files to the request pipeline.
+          app.UseStaticFiles();
 
-                // Uncomment the following line to add a route for porting Web API 2 controllers.
-                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
-            });
-        }
-    }
+          // Add MVC to the request pipeline.
+          app.UseMvc(routes =>
+          {
+              routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+
+              // Uncomment the following line to add a route for porting Web API 2 controllers.
+              // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+          });
+      }
+  }
 }
