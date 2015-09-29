0f25fb2dbd33efff293f61314f730777bc3c38d8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-30 00:12:38 +0200

    Add example for static and diagnostic pages

---
+++ DiagnosticStaticPages/Startup.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace DiagnosticPages
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseWelcomePage();
+        }
+    }
+}
