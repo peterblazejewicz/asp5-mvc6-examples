0eb1635a3301d966627bd7209f97253098885d14
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-10-10 23:20:05 +0200

    Remove old content

--- TagHelperSamples/Model/TestModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TagHelperSamples.Model
-{
-    public class TestModel
-    {
-        public int CurrentProgress { get; set; }
-        public string Message { get; set; }
-        public string Header { get; set; }
-    }
-}
