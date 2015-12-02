14eacbf9b56bb51e710616dd7d483e310335e15c
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-02 22:52:23 +0100

    Rewrite client side deps managment

--- SimpleViewComponent/src/SimpleViewComponent/gulpfile.js
+++ SimpleViewComponent/src/SimpleViewComponent/gulpfile.js
@@ -1,11 +1,12 @@
-/// <binding Clean='clean' />
+/* jshint node: true */
 "use strict";
 
 var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify");
+  uglify = require("gulp-uglify"),
+  mainBowerFiles = require("gulp-main-bower-files");
 
 var webroot = "./wwwroot/";
 
@@ -15,7 +16,8 @@ var paths = {
   css: webroot + "css/**/*.css",
   minCss: webroot + "css/**/*.min.css",
   concatJsDest: webroot + "js/site.min.js",
-  concatCssDest: webroot + "css/site.min.css"
+  concatCssDest: webroot + "css/site.min.css",
+  lib: webroot + "lib/"
 };
 
 gulp.task("clean:js", function (cb) {
@@ -24,6 +26,10 @@ gulp.task("clean:js", function (cb) {
 
 gulp.task("clean:css", function (cb) {
   rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean:lib", function(cb) {
+  rimraf(paths.lib, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
@@ -45,3 +51,9 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("bower", ["clean:lib"], function() {
+  return gulp.src("./bower.json")
+    .pipe(mainBowerFiles())
+    .pipe(gulp.dest(paths.lib));
+});
