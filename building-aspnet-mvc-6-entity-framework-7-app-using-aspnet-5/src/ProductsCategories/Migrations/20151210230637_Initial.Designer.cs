a6892746a84c230a434c9f955f50fdf8c9c1ba3d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-11 00:08:15 +0100

    Fix EF migration

---
+++ building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Migrations/20151210230637_Initial.Designer.cs
@@ -0,0 +1,51 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using ProductsCategories.Models;
+
+namespace ProductsCategories.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20151210230637_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16341");
+
+            modelBuilder.Entity("ProductsCategories.Models.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CategoryName");
+
+                    b.HasKey("CategoryId");
+                });
+
+            modelBuilder.Entity("ProductsCategories.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<int>("Price");
+
+                    b.Property<string>("ProductName");
+
+                    b.HasKey("ProductId");
+                });
+
+            modelBuilder.Entity("ProductsCategories.Models.Product", b =>
+                {
+                    b.HasOne("ProductsCategories.Models.Category")
+                        .WithMany()
+                        .HasForeignKey("CategoryId");
+                });
+        }
+    }
+}
---
+++ building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Migrations/20151210230637_Initial.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace ProductsCategories.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryId);
+                });
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryId = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    ProductName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Product_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Product");
+            migrationBuilder.DropTable("Category");
+        }
+    }
+}
