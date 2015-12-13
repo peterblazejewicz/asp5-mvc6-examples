b6bebefed5fcb05236cbe3ebcfea2946ccfbb184
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-13 17:26:50 +0100

    Scaffold CRUD for Product

--- building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Controllers/ProductsController.cs
+++ building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Controllers/ProductsController.cs
@@ -1,8 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using ProductsCategories.Models;
+using System.Collections.Generic;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,118 @@ namespace ProductsCategories.Controllers
 {
     public class ProductsController : Controller
     {
-        // GET: /<controller>/
+        private ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Products
         public IActionResult Index()
         {
+            var appDbContext = _context.Product.Include(p => p.Category);
+            return View(appDbContext.ToList());
+        }
+
+        // GET: Products/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Product product = _context.Product.Single(m => m.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Category");
             return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Product.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Category", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Product product = _context.Product.Single(m => m.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Category", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Category", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Product product = _context.Product.Single(m => m.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Product product = _context.Product.Single(m => m.ProductId == id);
+            _context.Product.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
