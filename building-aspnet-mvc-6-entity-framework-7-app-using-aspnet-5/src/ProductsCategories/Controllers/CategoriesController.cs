e2635f3201375eea8a7c951fa4efbd74e747b205
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-13 16:43:49 +0100

    Add scaffolded Categories CRUD

--- building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Controllers/CategoriesController.cs
+++ building-aspnet-mvc-6-entity-framework-7-app-using-aspnet-5/src/ProductsCategories/Controllers/CategoriesController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using ProductsCategories.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +8,114 @@ namespace ProductsCategories.Controllers
 {
     public class CategoriesController : Controller
     {
-        // GET: /<controller>/
+        private ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Categories
         public IActionResult Index()
         {
+            return View(_context.Category.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Category category = _context.Category.Single(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Category.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Category category = _context.Category.Single(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Category category = _context.Category.Single(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Category category = _context.Category.Single(m => m.CategoryId == id);
+            _context.Category.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
